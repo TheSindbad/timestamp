4ed7cbdcdf208a76d0e68802f79a1c2f1aaf9a87
TheSindbad <HramzovW2003@gmail.com>
2023-03-26 14:17:28 +0000

    fgghf

--- server.js
+++ server.js
@@ -13,22 +13,20 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// timestamp API
-app.get("/api/timestamp/:date?", (req, res) => {
+
+app.get("/api/:date?", (req, res) => {
   const givenDate = req.params.date;
   let date;
 
-  // check if no date provided
   if (!givenDate) {
     date = new Date();
   } else {
-    // check if unix time:
-    //    number string multiplied by 1 gives this number, data string gives NaN
+
     const checkUnix = givenDate * 1;
     date = isNaN(checkUnix) ? new Date(givenDate) : new Date(checkUnix);
   }
 
-  //check if valid format
+
   if (date == "Invalid Date") {
     res.json({ error: "Invalid Date" });
   } else {
@@ -38,6 +36,6 @@ app.get("/api/timestamp/:date?", (req, res) => {
   }
 });
 
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT||3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
